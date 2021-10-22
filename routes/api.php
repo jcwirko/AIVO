b08f1698706fa7ef4e3b73a751de8b21bb629816
juanblariza <juanblariza@gmail.com>
2021-10-22 13:45:08 -0300

    Agrega rutas api para consulta de artistas y albums

--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,7 @@
 <?php
 
-use App\Http\Controllers\BandaController;
+use App\Http\Controllers\MusicaController;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/spotify/artistas', [MusicaController::class, 'getArtistas']);
+Route::get('/spotify/discografia-by-banda', [MusicaController::class, 'getDiscografia']);
