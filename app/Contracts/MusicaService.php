a7ecb9c6abfc8dbce8e028c9b777f6c4c5b87dd7
juanblariza <juanblariza@gmail.com>
2021-10-22 13:49:21 -0300

    Crea musica services

--- app/Contracts/MusicaService.php
+++ app/Contracts/MusicaService.php
@@ -4,5 +4,6 @@ namespace App\Contracts;
 
 interface MusicaService
 {
-	public function obtenerDiscografia(string $banda);
+	public function getArtistas(string $banda);
+	public function getDiscografia(string $idBanda);
 }
